d3dbc2652842b54290fc0714e276d779dfed951e
Ramyadeivasigamanii <113896095+Ramyadeivasigamanii@users.noreply.github.com>
2023-11-02 15:53:41 +0530

    Add files via upload

---
+++ arithmetic.js
@@ -0,0 +1,18 @@
+// let num1=3
+// let num2=2
+// let result=num1%num2
+// console.log(result);
+
+// let num1=true
+// let num2=true 
+// let result=num1+num2
+// console.log(result);
+function a()
+let num=4
+let result=Math.pow(4,3)
+let r2= 4**4;
+let r3= 4*4;
+console.log(result);
+console.log(r2);
+console.log(r3);
+
